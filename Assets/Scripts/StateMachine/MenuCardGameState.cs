20344afee2bae5edff9b68bbb00c45fd6fd79fd1
Benjamin Giacobbi <benjamingiacobbi@gmail.com>
2020-11-22 15:28:52 -0600

    P02F Added Tooltip Option, minor tweaks

--- Assets/Scripts/StateMachine/MenuCardGameState.cs
+++ Assets/Scripts/StateMachine/MenuCardGameState.cs
@@ -11,18 +11,30 @@ public class MenuCardGameState : CardGameState
 
     [SerializeField] Button _startButton = null;
     [SerializeField] Button _quitButton = null;
+    [SerializeField] Button _guideButton = null;
     [SerializeField] Text _winsText = null;
     [SerializeField] Text _lossesText = null;
     [SerializeField] Text _ratioText = null;
     [SerializeField] AudioClip _titleMusic = null;
+    [SerializeField] TooltipUI _toolTips = null;
+    [SerializeField] Color _selectedColor;
+
+    Image _buttonImage = null;
 
     public override void Enter()
     {
+        if(!_buttonImage)
+        {
+            _buttonImage = _guideButton.GetComponent<Image>();
+            _buttonImage.color = _selectedColor;
+        }
+            
         _winsText.text = StateMachine.Player.Wins.ToString();
         _lossesText.text = StateMachine.Player.Losses.ToString();
         _ratioText.text = ((float)StateMachine.Player.Wins / StateMachine.Player.Losses != 0 ? StateMachine.Player.Losses : 1) + "W/L";
         _startButton.onClick.AddListener(StartGame);
         _quitButton.onClick.AddListener(QuitGame);
+        _guideButton.onClick.AddListener(GuideButtonState);
         MusicController.Instance.PlayMusic(_titleMusic, 0.5f);
         EnteredMenu?.Invoke();
     }
@@ -31,17 +43,27 @@ public class MenuCardGameState : CardGameState
     {
         _startButton.onClick.RemoveListener(StartGame);
         _quitButton.onClick.RemoveListener(QuitGame);
+        _guideButton.onClick.RemoveListener(GuideButtonState);
         ExitedMenu?.Invoke();
     }
 
-    public void StartGame()
+    private void StartGame()
     {
         StateMachine.ChangeState<SetupCardGameState>();
     }
 
-    public void QuitGame()
+    private void QuitGame()
     {
         SaveSystem.SaveRecordData(StateMachine.Player.Wins, StateMachine.Player.Losses);
         Application.Quit();
+    }
+
+    private void GuideButtonState()
+    {
+        _toolTips.ToggleToolTips();
+        if (_buttonImage.color == _selectedColor)
+            _buttonImage.color = Color.white;
+        else
+            _buttonImage.color = _selectedColor;
     }
 }
---
+++ Assets/TooltipUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TooltipUI : MonoBehaviour
+{
+    private Text _helpText = null;
+    private bool _help = true;
+
+    private void Awake()
+    {
+        _helpText = GetComponent<Text>();
+        _helpText.gameObject.SetActive(false);
+    }
+
+    public void ToggleToolTips()
+    {
+        _help = !_help;
+    }
+
+    public void ShowTooltip(string tooltip)
+    {
+        if(_help)
+        {
+            _helpText.text = tooltip;
+            _helpText.gameObject.SetActive(true);
+        }
+        
+    }
+
+    public void HideTooltip()
+    {
+        if(_help)
+            _helpText.gameObject.SetActive(false);
+    }
+}
